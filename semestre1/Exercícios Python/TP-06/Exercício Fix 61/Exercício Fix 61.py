91cfbe7d1e899b4918229248603db5ebaf4e9ed0
GRAZIELLA SOUZA <1051392411017@fatec>
2024-05-16 10:01:14 -0300

    Iniciando TP-06

---
+++ semestre1/Exercícios Python/TP-06/Exercício Fix 61/Exercício Fix 61.py
@@ -0,0 +1,7 @@
+import os
+
+diretorio_corrente = os.getcwd()
+print("Você está no diretório:")
+print(diretorio_corrente)
+
+arq = open('C:\\Users\\1051392411017\\Documents\\fatec\\semestre1\\Exercícios Python\\TP-06\\Exercício Fix 61\\Mensagem.txt', 'r', encoding="utf-8")
