6754757ae2e79550324676fa3e49735cc0de5be7
grazisouza1 <graziisouza0127@gmail.com>
2024-05-17 12:17:04 -0300

    Primeiro teste arq.read()

--- semestre1/Exercícios Python/TP-06/Exercício Fix 61/Exercício Fix 61.py
+++ semestre1/Exercícios Python/TP-06/Exercício Fix 61/Exercício Fix 61.py
@@ -4,4 +4,9 @@ diretorio_corrente = os.getcwd()
 print("Você está no diretório:")
 print(diretorio_corrente)
 
-arq = open('C:\\Users\\1051392411017\\Documents\\fatec\\semestre1\\Exercícios Python\\TP-06\\Exercício Fix 61\\Mensagem.txt', 'r', encoding="utf-8")
+arq = open(r"C:\Users\Juciele\Documents\fatec\semestre1\Exercícios Python\TP-06\Exercício Fix 61\Mensagem.txt", encoding="utf-8")
+print(arq)
+
+text = arq.read()
+print(text)
+
