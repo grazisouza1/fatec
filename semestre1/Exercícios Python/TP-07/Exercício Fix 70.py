1b5f4b5f001ebbd91773448bbcda45e630e2721d
grazisouza1 <graziisouza0127@gmail.com>
2024-06-05 10:51:57 -0300

    Corrigindo Exercício Fix 70

--- semestre1/Exercícios Python/TP-07/Exercício Fix 70.py
+++ semestre1/Exercícios Python/TP-07/Exercício Fix 70.py
@@ -1,10 +1,30 @@
+con = {'A': 'APROVADO',
+       'B': 'APROVADO',
+       'C': 'EXAME',
+       'D': 'DP'}
 
+nome = input("Digite seu nome")
+a = float(input("Digite sua 1º nota: "))
+b = float(input("Digite sua 2º nota: "))
+m = (a*4 + b*6) / 10
+print()
 
-nome = str(input("Digite seu nome:"))
-n1 = float(input("Digite a nota da sua primeira avaliação"))
-n2 = float(input("Digite a nota da sua segunda avaliação"))
-m = (n1 + n2) / 2 
+print("TABELA DE ATRIBUIÇÃO:")
+for chave, valor in con.items():
+    print(chave,valor)
+print()
 
-conceitos = {
-            
-            }
+for chave, valor in con.items():
+    if m <= 10 and m >= 9:
+       conceito = 'A'
+    elif m <= 9 and m >= 7:
+        conceito = 'B'
+    elif m <= 7 and m >= 3:
+        conceito = 'C'
+    else:
+        conceito = 'D'
+
+print("ALUNO: ", nome)
+print("RA: 1051392411017")
+print()
+print("Segundo a tabela, seu conceito é ", con[conceito], ", e sua média foi de ", m)
