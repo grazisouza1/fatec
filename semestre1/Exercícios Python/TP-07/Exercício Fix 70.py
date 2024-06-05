e6af309d7c4ab2d9a9f2fd3da165e2807799c76e
grazisouza1 <graziisouza0127@gmail.com>
2024-06-05 10:51:05 -0300

    Finalizando Exercício Fix 70

---
+++ semestre1/Exercícios Python/TP-07/Exercício Fix 70.py
@@ -0,0 +1,10 @@
+
+
+nome = str(input("Digite seu nome:"))
+n1 = float(input("Digite a nota da sua primeira avaliação"))
+n2 = float(input("Digite a nota da sua segunda avaliação"))
+m = (n1 + n2) / 2 
+
+conceitos = {
+            
+            }
