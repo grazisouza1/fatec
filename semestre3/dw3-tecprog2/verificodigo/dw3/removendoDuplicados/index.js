f327339753a1786d313fdf1eb4d279ea3400756b
Graziella Souza <graziisouza0127@gmail.com>
2025-04-01 23:41:15 -0300

    Second delivery from verificódigo

--- semestre3/dw3-tecprog2/verificodigo/dw3/removendoDuplicados/index.js
+++ semestre3/dw3-tecprog2/verificodigo/dw3/removendoDuplicados/index.js
@@ -2,15 +2,29 @@ import express from "express";
 
 const app = express();
 
-let removeDuplicados = () => {};
+let removeDuplicados = (palavra) => {
+  let arrayPalavra = palavra.split("");
+  for (let i = 0; i < arrayPalavra.length - 1; i++) {
+    for (let j = i + 1; j < arrayPalavra.length; j++) {
+      if (arrayPalavra[i] === arrayPalavra[j]) {
+        arrayPalavra.splice(j, 1);
+        j--;
+      }
+    }
+  }
+  return arrayPalavra.join("");
+};
 
 app.get("/remove-duplicates/:palavra", (Request, Response) => {
   let palavra = Request.params.palavra;
-  let palavraMod = removeDuplicados();
+  let palavraMod = removeDuplicados(palavra);
 
   console.log(palavra);
-  Response.send(`Palavra sem caracteres repetidos: ${palavraMod}`);
+  Response.send(`
+    Palavra: ${palavra} <br>
+    Palavra sem caracteres repetidos: ${palavraMod}
+    `);
 });
 
-let PORT = 3001;
+let PORT = 3000;
 app.listen(PORT, console.log(`Runing on port ${PORT}`));
