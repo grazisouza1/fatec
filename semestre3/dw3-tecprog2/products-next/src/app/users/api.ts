56f0019d7dce18669331bbe31c7bbe79aa75850b
Graziella Souza <graziisouza0127@gmail.com>
2025-04-25 07:58:52 -0300

    Changing products-next

--- semestre3/dw3-tecprog2/products-next/src/app/users/api.ts
+++
@@ -1,9 +0,0 @@
-import { IUser } from "@/interfaces/user.interface"
-
-async function carregarUsers():Promise<IUser[]> {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users')
-    const json = await response.json()
-    return json
-}
-
-  export default carregarUsers
