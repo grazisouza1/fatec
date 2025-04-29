d738c0534403234282447de0ab8c2c0969556bc7
Graziella Souza <graziisouza0127@gmail.com>
2025-04-29 15:45:16 -0300

    Adding somaDois exercise

---
+++ semestre3/dw3-tecprog2/verificodigo/tec2/somadois/index.ts
@@ -0,0 +1,14 @@
+function somaDois(nums: number[], target: number) {
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const complemento = target - nums[i];
+    if (map.has(complemento)) {
+      return [map.get(complemento), i];
+    }
+    map.set(nums[i], i);
+  }
+  return [];
+}
+
+somaDois([2, 5, 6, 2, 1, 0, 5], 11);
